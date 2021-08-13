f2ec422afc9b49f328dc172cb43c54f8ca6c836d
NotteGatto <ast2605@gmail.com>
2021-08-14 01:29:45 +0600

    the second commit print integers from 1 to 100

--- src/hwgit/HWGit.java
+++ src/hwgit/HWGit.java
@@ -3,8 +3,9 @@ package hwgit;
 public class HWGit {
 
 	public static void main(String[] args) {
-		for (int i = 1; i < 11; i++) {
-			System.out.println(i);
+		for (int i = 1; i < 101; i++) {
+			
+				System.out.println(i + " ");
 		}
 
 	}
